eaf02f0b112c169e897ef644e96e1e195fadae91
Aleix Pol <aleixpol@kde.org>
2020-11-13 00:52:28 +0100

    Implement startup notifiers

--- xdgactivationv1.cpp
+++ xdgactivationv1.cpp
@@ -14,6 +14,7 @@
 #include <KWaylandServer/xdgactivation_v1_interface.h>
 #include "workspace.h"
 #include "utils.h"
+#include "effects.h"
 #include "abstract_client.h"
 
 Q_DECLARE_LOGGING_CATEGORY(KWIN_ACTIVATION)
@@ -24,14 +25,57 @@ using namespace KWaylandServer;
 namespace KWin
 {
 
+class ActivationTracker : public QObject
+{
+public:
+    void update(const QString &token, const QString &appId) {
+        const auto iconName = AbstractClient::iconFromDesktopFile(appId);
+        const auto icon = QIcon::fromTheme(iconName, QIcon::fromTheme(QStringLiteral("system-run")));
+
+        const auto it = m_activating.find(token);
+        if (it != m_activating.end()) {
+            *it = icon;
+            Q_EMIT effects->startupAdded(token.toUtf8(), icon);
+        } else {
+            m_activating[token] = icon;
+            Q_EMIT effects->startupChanged(token.toUtf8(), icon);
+        }
+    }
+
+    void remove(const QString &token) {
+        auto removed = m_activating.remove(token);
+        if (removed) {
+            effects->startupRemoved(token);
+        }
+    }
+
+    static ActivationTracker* self() {
+        static ActivationTracker* s_self = nullptr;
+
+        if (!s_self) {
+            s_self = new ActivationTracker;
+        }
+        return s_self;
+    }
+
+private:
+    QHash<QString, QIcon> m_activating;
+};
+
 XdgActivationV1Integration::XdgActivationV1Integration(QObject *parent)
     : QObject(parent)
 {
     auto activation = waylandServer()->display()->createXdgActivationV1(waylandServer()->display());
 
     Workspace *ws = Workspace::self();
-    connect(ws, &Workspace::clientActivated, this, [this] {
+    connect(ws, &Workspace::clientActivated, this, [this] (AbstractClient *client) {
         m_currentActivationToken.reset();
+
+        auto waylandClient = client->surface()->client();
+        const auto token = waylandClient->property("activationToken").toString();
+        if (!token.isEmpty()) {
+            ActivationTracker::self()->remove(token);
+        }
     });
     activation->setActivationTokenCreator([this] (KWaylandServer::SurfaceInterface *surface, uint serial, KWaylandServer::SeatInterface *seat) -> QString {
         Workspace *ws = Workspace::self();
@@ -44,6 +88,7 @@ XdgActivationV1Integration::XdgActivationV1Integration(QObject *parent)
         const auto newToken = QStringLiteral("kwin-%1").arg(++i);
 
         m_currentActivationToken.reset(new ActivationToken{ newToken, surface, serial, seat, {} });
+        ActivationTracker::self()->update(newToken, {});
 
         return newToken;
     });
@@ -55,6 +100,8 @@ XdgActivationV1Integration::XdgActivationV1Integration(QObject *parent)
 
         qCDebug(KWIN_ACTIVATION) << "according to" << client << "token:" << token << "is launching" << appId;
         m_currentActivationToken->applicationId = appId;
+
+        ActivationTracker::self()->update(token, appId);
     });
     connect(activation, &KWaylandServer::XdgActivationV1Interface::activate, this, &XdgActivationV1Integration::activateSurface);
     connect(activation, &KWaylandServer::XdgActivationV1Interface::setActivation, this, [this] (ClientConnection *c, const QString &token) {
